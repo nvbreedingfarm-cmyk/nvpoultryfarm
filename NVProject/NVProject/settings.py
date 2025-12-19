281e2ded0bbba01dfdd196f9fb04b8a8d9d373f9
nvbreedingfarm-cmyk <nvbreedingfarm@gmail.com>
2025-12-20 01:05:11 +0530

    Updated models

--- NVProject/NVProject/settings.py
+++ NVProject/NVProject/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'NVProject.wsgi.application'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'NV Poultry',
+#         'NAME': 'NV',
 #         'USER': 'postgres',
 #         'PASSWORD':'1234',
 #         'HOST':'localhost',
